6f0982ed02d8a84b0ecea53f1d5a6101bb7fb216
MirshodAslonov <mirshodaslonov001@gmail.com>
2025-10-31 19:03:11 +0500

    m

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -11,7 +11,7 @@ class BotController extends Controller
 {
     public function start(Request $request)
 {
-    $botToken = env('TELEGRAM_BOT_TOKEN');
+    $botToken = "8302414123:AAEoTUMkOP-o-kZt-7IRUDvLWWzStk4LAL8";
     $data = $request->all();
     $telegramId = (string) ($data['message']['from']['id'] ?? null);
 
