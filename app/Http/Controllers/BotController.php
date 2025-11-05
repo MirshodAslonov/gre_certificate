3da0cd5d16028acfbf9892d0a4b6fc0e33e33d1b
MirshodAslonov <mirshodaslonov001@gmail.com>
2025-11-05 22:23:09 +0500

    z

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -121,8 +121,8 @@ class BotController extends Controller
         'chat_id' => $groupId,
         'user_id' => $telegramId,
     ]);
-    $user_id = User::where('telegram_id', (string)$telegramId)->first();
-    dd($user_id,$telegramId);
+    $user_id = User::where('telegram_id', (string)$telegramId)->first()->id;
+    
     $invite_link  = UserSubscription::where('user_id',$user_id)
         ->orderBy('id','desc')
         ->first()
